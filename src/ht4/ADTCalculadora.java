21f9f582d8c257ce354003c1596b5922809d6f41
Juliantrator <lop14684@uvg.edu.gt>
2016-08-22 09:58:23 -0600

    Update ADTCalculadora.java

--- src/ht4/ADTCalculadora.java
+++ src/ht4/ADTCalculadora.java
@@ -8,7 +8,7 @@ import java.io.FileNotFoundException;
 
 public interface ADTCalculadora<P> {
 	public void readFile(String file) throws FileNotFoundException, Exception;
-	public double calcular() throws Exception;
+	public double calcular() throw Exception;
 	
 	
 }
