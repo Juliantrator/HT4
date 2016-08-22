cee1e81e650b235671571ea3ebaeba408c6a8a4e
Juliantrator <lop14684@uvg.edu.gt>
2016-08-22 10:01:22 -0600

    Update ADTStack.java

--- src/ht4/ADTStack.java
+++ src/ht4/ADTStack.java
@@ -1,7 +1,8 @@
-
-/**
- * @author Isaac Rodriguez, Julian Lopez
- *
+/*
+ * Universidad del Valle de Guatemala
+ * 22-08-16
+ * @author Isaac Rodriguez 
+ *@author Julian Lopez
  */
 public interface ADTStack<E> {
 	public void empty();
