eb2c76533e8c074d6740b317a957cd94124269ba
Juliantrator <lop14684@uvg.edu.gt>
2016-08-22 10:00:06 -0600

    Update ADTCalculadora.java

--- src/ht4/ADTCalculadora.java
+++ src/ht4/ADTCalculadora.java
@@ -1,8 +1,11 @@
+
 import java.io.FileNotFoundException;
 
 /**
+ * Universidad del Valle de Guatemala
+ * 22-08-16
  * @author Isaac Rodriguez 
- *
+ *@author Julian Lopez
  */
 import java.io.FileNotFoundException;
 
